c8f111e170ddb0b8ffec44b1e8e8607268ce169d
loveKino <842914439@qq.com>
2017-09-28 16:39:40 +0800

    doc site

--- docSite/lib/pageView/indexPage.js
+++ docSite/lib/pageView/indexPage.js
@@ -11,6 +11,10 @@ let {
     onSignalType
 } = require('../../../lib/util/signal');
 let {
+    styles
+} = require('../../../lib/util/helper');
+
+let {
     // SIGNAL CONSTANTS
 } = require('../signals');
 
@@ -19,6 +23,8 @@ let {
 let FunctionBar = require('../../../lib/view/header/functionBar');
 let Hn = require('../../../lib/view/layout/hn');
 let Vn = require('../../../lib/view/layout/vn');
+let Block = require('../../../lib/view/block/block');
+let Flat = require('../../../lib/view/block/flat');
 
 /**
  * // some common signals
@@ -44,11 +50,14 @@ let Vn = require('../../../lib/view/layout/vn');
  *     demo: n(Button, {onsignal: onSignalType('click', deliver(ctx, SIGNAL_TYPE))}, 'save')
  */
 
-module.exports = SimplePager(lumineView(({}, ctx) => {
+module.exports = SimplePager(lumineView(({
+    props
+}, ctx) => {
     return n(Vn, [
         n(FunctionBar, {
             title: 'kabanery lumine',
             rightLogos: [
+                n('div', 'view demo'),
                 n('a href="https://github.com/LoveKino/kabanery-lumine" target="_blank"', {
                     style: {
                         color: 'white',
@@ -58,8 +67,68 @@ module.exports = SimplePager(lumineView(({}, ctx) => {
                         'line-height': '30px'
                     }
                 }, 'github')
-            ]
-        })
+            ],
+
+            onsignal: onSignalType('click', (signal) => {
+                if (signal.data.sourceType === 'rightLogos') {
+                    if (signal.data.index === 0) {
+                        window.location.href = '?page=viewDemo';
+                    }
+                }
+            })
+        }),
+
+        n('div', {
+            style: {
+                marginTop: 30,
+                padding: 20,
+                textAlign: 'center'
+            }
+        }, [
+            n(Block, {
+                style: {
+                    display: 'inline-block',
+                    fontSize: 30,
+                    padding: 12,
+                    marginBottom: 18
+                }
+            }, [
+                'What you imagine, what you get.'
+            ]),
+            n('br'),
+
+            n(Block, {
+                style: {
+                    display: 'inline-block',
+                    fontSize: 20,
+                    padding: 8
+                }
+            }, 'DSL driven, Data migration from signal system, Common views integration Front End framework.')
+        ]),
+
+        n(Vn, {
+            style: {
+                container: {
+                    padding: 8
+                }
+            }
+        }, [
+            n(Flat, {
+                style: {
+                    fontSize: 20,
+                    fontWeight: 'bold'
+                }
+            }, 'main features'),
+            n(Flat, [
+                n(Flat, 'A simple way to define view'),
+                n(Flat, 'DSL driven, simple DSL for specific job.'),
+                n(Flat, 'Data migration system'),
+                n(Flat, 'Common views'),
+                n(Flat, 'Theme system'),
+                n(Flat, 'Skelton tools'),
+                n(Flat, 'Other tools')
+            ])
+        ])
     ])
 }, {
     defaultProps: {}
